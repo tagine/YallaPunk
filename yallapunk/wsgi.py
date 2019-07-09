7264bf0d3d26913098967bf32ec6595b5e751342
dtw45 <derekwladyka@gmail.com>
2019-07-09 08:23:53 -0400

    rename yalltail -> yallapunk

--- yallapunk/wsgi.py
+++ yallapunk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yalltail project.
+WSGI config for yallapunk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yalltail.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yallapunk.settings.dev")
 
 application = get_wsgi_application()
