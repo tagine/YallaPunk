6ecb21d8fc2d1866f602b0845fbe9fd49c1fc485
dtw45 <derek.wladyka@gmail.com>
2019-07-27 17:36:15 -0400

    Update settings to get ready for prod deployment

--- yallapunk/settings/dev.py
+++ yallapunk/settings/dev.py
@@ -11,6 +11,15 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 try:
     from .local import *
--- yallapunk/settings/production.py
+++ yallapunk/settings/production.py
@@ -1,6 +1,30 @@
 from .base import *
 
+from yallapunk.config import config_loader
+
+config = config_loader('production')
+
 DEBUG = False
+
+SECRET_KEY = config['SECRET_KEY']
+
+ALLOWED_HOSTS = config['ALLOWED_HOSTS']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+db = config['DB']
+
+DATABASES = {
+    'default': {
+        'NAME': db['NAME'],
+        'ENGINE': db['ENGINE'],
+        'HOST': db['HOST'],
+        'PORT': db['PORT'],
+        'USER': db['USER'],
+        'PASSWORD': db['PASSWORD'],
+    }
+}
 
 try:
     from .local import *
