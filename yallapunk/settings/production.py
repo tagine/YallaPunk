2086acffdce6e2d1f2827c510c1e9550bdfa2bf6
dtw45 <derek.wladyka@gmail.com>
2019-08-11 08:29:18 -0400

    add support for static s3 files

--- yallapunk/settings/production.py
+++ yallapunk/settings/production.py
@@ -26,6 +26,17 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS.extend([
+    'django_s3_storage',
+])
+
+# django_s3_settings
+STATIC_S3_BUCKET = config['S3']['STATIC']
+STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+AWS_S3_BUCKET_NAME_STATIC = STATIC_S3_BUCKET
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % STATIC_S3_BUCKET
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
 try:
     from .local import *
 except ImportError:
